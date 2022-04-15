c428bae3492cfcae54301ae8126c78d0fca264f5
Philippe TONY <philippe.julien.tony@laposte.net>
2022-04-15 21:30:54 +0200

    feat: early frontend structure

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,17 @@
 import './App.css';
 
+import {QueryClient, QueryClientProvider} from 'react-query';
+import Caisse from './Caisse';
+
+
+const queryClient = new QueryClient()
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="caisse">
+      <QueryClientProvider client={queryClient}>
+        <Caisse />
+      </QueryClientProvider>
     </div>
   );
 }
---
+++ frontend/src/Caisse.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './Caisse.scss'
+
+export default function Caisse() {
+    return <div>Hello world ! </div>
+}
