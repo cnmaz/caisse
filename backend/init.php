78c6e6f62966dd96173054744a0a994d2fc510db
Philippe TONY <philippe.julien.tony@laposte.net>
2022-04-16 10:00:04 +0200

    feat: Added more sample products

--- backend/init.php
+++ backend/init.php
@@ -16,20 +16,30 @@ $cakes->label = "Gateaux";
 $cakes->ordre = 2;
 R::store($cakes);
 
-function product($id, $label, $category, $ordre)
+function product($id, $label, $category, $price, $ordre)
 {
     $product = R::load("product", $id);
     $product->label = $label;
     $product->category = $category;
+    $product->price = $price;
     $product->ordre = $ordre;
     R::store($product);
 }
 
-product(1, "Bière", $drinks, 1);
-product(2, "Christaline (50cl)", $drinks, 2);
-product(3, "San pellegrino (50cl)", $drinks, 3);
-product(4, "Coca-Zero (33cl)", $drinks, 4);
-product(5, "Coca (33cl)", $drinks, 5);
-product(5, "Fanta (33cl)", $drinks, 6);
+product(1, "Bière", $drinks, 2.5, 1);
+product(2, "Christaline (50cl)", $drinks, .5, 2);
+product(3, "San pellegrino (50cl)", $drinks, 1, 3);
+product(4, "Coca-Zero (33cl)", $drinks, 1, 4);
+product(5, "Coca (33cl)", $drinks, 1, 5);
+product(6, "Fanta (33cl)", $drinks, 1, 6);
+product(7, "Tropico pomme banane (33cl)", $drinks, 1, 6);
+product(8, "Tropico exotique (33cl)", $drinks, 1, 6);
+product(9, "Tropico orange (33cl)", $drinks, 1, 6);
+
+product(10, "Part tarte aux pommes", $cakes, 2, 1);
+product(11, "Part flan", $cakes, 2, 2);
+product(12, "Part cake chocolat", $cakes, 2, 3);
+product(13, "Part cake pépites chcolat", $cakes, 2, 3);
+product(14, "Part gateau citron", $cakes, 2, 3);
 
 R::close();
