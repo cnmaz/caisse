870255d77cf64dcc203aa3d0fd826295e5df2173
Philippe TONY <philippe.julien.tony@laposte.net>
2022-04-16 11:08:28 +0200

    feat: added basic actions buttons

--- frontend/src/components/Actions.jsx
+++ frontend/src/components/Actions.jsx
@@ -1,6 +1,33 @@
 
-export default function Actions() {
+import { Button } from '@mui/material';
+import './Actions.scss';
+import { CartType } from '../hooks/useCart';
+
+export default function Actions({ cart }) {
+
     return <div elevation="2" className="actions-container">
         <h1>Actions</h1>
+        <div className="actions">
+            <Button variant="outlined">
+                Réglement CB
+            </Button>
+            <Button variant="outlined">
+                Réglement Espèces
+            </Button>
+            <Button variant="outlined">
+                Annulation
+            </Button>
+            <Button variant="outlined">
+                Mise en attent
+            </Button>
+            <Button variant="outlined">
+                Historique ventes
+            </Button>
+            <Button variant="outlined">
+                Client suivant
+            </Button>
+        </div>
     </div>
 }
+
+Actions.propTypes = { cart: CartType }
