338345794722c1a67f003bc2a2c601f552483934
Philippe TONY <philippe.julien.tony@laposte.net>
2022-05-05 13:28:45 +0200

    feat: Mock is a good one

--- pos-driver/app.py
+++ pos-driver/app.py
@@ -88,11 +88,11 @@ def open_mock_serial():
     serial_mock = MagicMock()
     serial_mock.read = Mock()
     # Good one
-    # serial_mock.read.side_effect = ['\06'.encode(), '\06'.encode(
-    # ), '\05'.encode(), '\x0201000000001B978          \x032'.encode(), '\x04'.encode()]
-    # Bad one
     serial_mock.read.side_effect = ['\06'.encode(), '\06'.encode(
-    ), '\05'.encode(), '\x02017000000010978          \x032'.encode(), '\x04'.encode()]
+    ), '\05'.encode(), '\x0201000000001B978          \x032'.encode(), '\x04'.encode()]
+    # Bad one
+    # serial_mock.read.side_effect = ['\06'.encode(), '\06'.encode(
+    # ), '\05'.encode(), '\x02017000000010978          \x032'.encode(), '\x04'.encode()]
     return serial_mock
 
 
