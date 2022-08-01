e7b1cb93944070e78ec11c6506b3043cd2ff4f65
Philippe TONY <philippe.julien.tony@laposte.net>
2022-08-01 21:48:35 +0200

    feat: add overlay actions for product grid

---
+++ frontend/src/components/OverlayActions.jsx
@@ -0,0 +1,20 @@
+
+import { Button } from '@mui/material';
+import './OverlayActions.scss';
+import { CartStates, CartType } from '../hooks/useCart';
+
+export default function Actions({ cart, setActiveTab }) {
+
+    return <div elevation="2" className="overlay-actions-container">
+        <div className="overlay-actions">
+            <Button variant="outlined" disabled={false} onClick={() => { cart?.historiqueVentes(); setActiveTab('historique') }}>
+                Historique ventes
+            </Button>
+            <Button variant="contained" disabled={cart.state !== CartStates.HistoriqueVentes && cart.state !== CartStates.Annule && cart.state !== CartStates.Paye && cart.state !== CartStates.EnAttente} onClick={() => { cart?.nouveauClient(); setActiveTab("produits") }}>
+                Nouveau client
+            </Button>
+        </div>
+    </div>
+}
+
+Actions.propTypes = { cart: CartType }
--- frontend/src/components/ProductGrid.jsx
+++ frontend/src/components/ProductGrid.jsx
@@ -6,6 +6,7 @@ import { func } from 'prop-types';
 import { useQuery } from 'react-query';
 import './ProductGrid.scss'
 import { CartStates, CartType } from '../hooks/useCart';
+import OverlayActions from './OverlayActions'
 
 export default function ProductGrid({ onClickProduct, cart }) {
     const { data: categories, loading: loadingCategories, error: errorCategories } = useQuery(["category"], () =>
@@ -32,35 +33,42 @@ export default function ProductGrid({ onClickProduct, cart }) {
     }
 
     return <div className="product-grid-container">
-        {categories
-            ?.sort((a,b) => a?.ordre -b?.ordre)
-            ?.map(category => {
-                const categoryProducts = products
-                    ?.filter(product => product.category_id === category.id)
-                    ?.filter(product => product.active === "1")
-                    ?.map(product => ({ ...product, price: parseFloat(product.price) }));
-                if (categoryProducts?.length > 0) {
-                    return (<div key={category?.id}>
-                        <div className="category-label">{category.label}</div>
-                        <ul>
-                            {categoryProducts
-                                ?.sort((a,b) => a?.ordre -b?.ordre)
-                                ?.map(product =>
-                                    <li key={product?.id}>
-                                        <Badge badgeContent={numberOfProducts(product.id)} color="primary" overlap="circular" key={product.id}>
-                                            <Button variant="outlined" className="product" onClick={() => onClickProduct(product)} disabled={cart.state !== CartStates.Saisie && cart.state !== CartStates.Annulation}>
-                                                {product?.label}
-                                            </Button>
-                                        </Badge>
-                                    </li>
-                                )}
-                        </ul>
-                    </div>)
-                } else {
-                    return <></>
+        {!(cart.state !== CartStates.HistoriqueVentes && cart.state !== CartStates.Annule && cart.state !== CartStates.Paye && cart.state !== CartStates.EnAttente) && <div className="product-grid-overlay">
+            <div>
+                <OverlayActions cart={cart} />
+            </div>
+        </div>}
+        <div className="product-grid">
+            {categories
+                ?.sort((a, b) => a?.ordre - b?.ordre)
+                ?.map(category => {
+                    const categoryProducts = products
+                        ?.filter(product => product.category_id === category.id)
+                        ?.filter(product => product.active === "1")
+                        ?.map(product => ({ ...product, price: parseFloat(product.price) }));
+                    if (categoryProducts?.length > 0) {
+                        return (<div key={category?.id}>
+                            <div className="category-label">{category.label}</div>
+                            <ul>
+                                {categoryProducts
+                                    ?.sort((a, b) => a?.ordre - b?.ordre)
+                                    ?.map(product =>
+                                        <li key={product?.id}>
+                                            <Badge badgeContent={numberOfProducts(product.id)} color="primary" overlap="circular" key={product.id}>
+                                                <Button variant="outlined" className="product" onClick={() => onClickProduct(product)} disabled={cart.state !== CartStates.Saisie && cart.state !== CartStates.Annulation}>
+                                                    {product?.label}
+                                                </Button>
+                                            </Badge>
+                                        </li>
+                                    )}
+                            </ul>
+                        </div>)
+                    } else {
+                        return <></>
+                    }
                 }
-            }
-            )}
+                )}
+        </div>
     </div>
 }
 
