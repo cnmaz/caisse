1fbbfac4047c6ec91cb3520e2ed77552ebbeb9f8
Philippe TONY <philippe.julien.tony@laposte.net>
2022-09-02 07:36:36 +0200

    fix: php7 compatibility

--- backend/index.php
+++ backend/index.php
@@ -4,14 +4,18 @@ R::setup('sqlite:./dbfile.db');
 
 $uri = $_SERVER['REQUEST_URI'];
 
+function starts_with ( $haystack, $needle ) {
+    return strpos( $haystack , $needle ) === 0;
+}
+
 if ($uri == "/init") {
     require "init.php";
 } elseif ($uri == "/debug") {
     require "debug.php";
-} elseif (str_starts_with($uri,"/product")) {
+} elseif (starts_with($uri,"/product")) {
     require "product.php";
-} elseif (str_starts_with($uri,"/category")) {
+} elseif (starts_with($uri,"/category")) {
     require "category.php";
-} elseif (str_starts_with($uri,"/sale")) {
+} elseif (starts_with($uri,"/sale")) {
     require "sale.php";
 }
